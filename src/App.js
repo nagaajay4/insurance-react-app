72796fb89f0e87402ebca4b90014a1a78de368a0
Naga Ajay K <nagaajay9535@gmail.com>
2023-10-02 12:49:11 +0530

    React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import AddInsuranceUser from "./components/AddInsuranceUser";
+import ShowInsurancePremium from './components/ShowInsurancePremium';
+import VerifyPurchase from "./components/VerifyPurchase";
+import ThanksPage from "./components/ThanksPage";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<AddInsuranceUser />} />
+                    <Route path="/cart_premium" element={<ShowInsurancePremium />} />
+                    <Route path="/verify_purchase" element={<VerifyPurchase />} />
+                    <Route path="/thankspage" element={<ThanksPage />} />
+                </Routes>
+            </BrowserRouter>
 
+            {/* <AddInsuranceUser></AddInsuranceUser> */}
+            {/* <ShowInsurancePremium></ShowInsurancePremium> */}
+            {/* <VerifyPurchase></VerifyPurchase> */}
+        </div>
+    )
+}
 export default App;
